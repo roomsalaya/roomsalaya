1c3df088975d91378e67d0c09eba039a8381f0d9
Supunsapoon <supunsapoon@gmail.com>
2024-07-24 14:53:24 +0700

    First time to commit

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,12 @@ import PaymentHistory205 from './components/PaymentHistory205';
 import Allowpayment204 from './components/Allowpayment204';
 import Allowpayment205 from './components/Allowpayment205';
 import HouseInfo205 from './components/HouseInfo205';
+import UserPage206 from './components/UserPage206';
+import HouseInfo206 from './components/HouseInfo206';
+import Room206 from './components/Room206';
+import BankTransferForm206 from './components/BankTransferForm206';
+import PaymentHistory206 from './components/PaymentHistory206';
+import Allowpayment206 from './components/Allowpayment206';
 
 // Lazy load components
 const AdminPage = lazy(() => import('./components/AdminPage'));
@@ -60,16 +66,19 @@ const App: React.FC = () => {
                     <Route path='/UserPage202' element={<UserPage202 />} />
                     <Route path='/UserPage204' element={<UserPage204 />} />
                     <Route path='/UserPage205' element={<UserPage205 />} />
+                    <Route path='/UserPage206' element={<UserPage206 />} />
                     
                     <Route path="/houseinfo201" element={user ? <HouseInfo201 /> : <Navigate to="/login" />} />
                     <Route path="/houseinfo202" element={user ? <HouseInfo202 /> : <Navigate to="/login" />} />
                     <Route path="/houseinfo204" element={user ? <HouseInfo204 /> : <Navigate to="/login" />} />
                     <Route path="/houseinfo205" element={user ? <HouseInfo205 /> : <Navigate to="/login" />} />
+                    <Route path="/houseinfo206" element={user ? <HouseInfo206 /> : <Navigate to="/login" />} />
                     
                     <Route path="/room201" element={user ? <Room201 /> : <Navigate to="/login" />} />
                     <Route path="/room202" element={user ? <Room202 /> : <Navigate to="/login" />} />
                     <Route path="/room204" element={user ? <Room204 /> : <Navigate to="/login" />} />
                     <Route path="/room205" element={user ? <Room205 /> : <Navigate to="/login" />} />
+                    <Route path="/room206" element={user ? <Room206 /> : <Navigate to="/login" />} />
                     
                     <Route path="/" element={<Home />} />
 
@@ -77,16 +86,19 @@ const App: React.FC = () => {
                     <Route path='/bank202' element={<BankTransferForm202 />} />
                     <Route path='/bank204' element={<BankTransferForm204 />} />
                     <Route path='/bank205' element={<BankTransferForm205 />} />
+                    <Route path='/bank206' element={<BankTransferForm206 />} />
 
                     <Route path='/paymenthistory201' element={<PaymentHistory201 />} />
                     <Route path='/paymenthistory202' element={<PaymentHistory202 />} />
                     <Route path='/paymenthistory204' element={<PaymentHistory204 />} />
                     <Route path='/paymenthistory205' element={<PaymentHistory205 />} />
+                    <Route path='/paymenthistory206' element={<PaymentHistory206 />} />
                     
                     <Route path='/allowpayment201' element={<Allowpayment201 />} />
                     <Route path='/allowpayment202' element={<Allowpayment202 />} />
                     <Route path='/allowpayment204' element={<Allowpayment204 />} />
                     <Route path='/allowpayment205' element={<Allowpayment205 />} />
+                    <Route path='/allowpayment206' element={<Allowpayment206 />} />
                     
                     <Route path='/allowpayment' element={<Allowpayment />} />
                     
--- src/components/AdminPage.tsx
+++ src/components/AdminPage.tsx
@@ -3,6 +3,9 @@ import './AdminPage.css';
 import HouseInfo201 from './HouseInfo201';
 import AppMenuAdmin from './AppMenuAdmin';
 import HouseInfo202 from './HouseInfo202';
+import HouseInfo204 from './HouseInfo204';
+import HouseInfo205 from './HouseInfo205';
+import HouseInfo206 from './HouseInfo206';
 
 const AdminPage: React.FC = () => {
     const [isHouseInfo201Visible, setHouseInfo201Visible] = useState(false);
@@ -30,6 +33,21 @@ const AdminPage: React.FC = () => {
                 {isHouseInfo202Visible ? 'ปิดข้อมูล 202' : 'เปิดข้อมูล 202'}
             </button>
             {isHouseInfo202Visible && <HouseInfo202 />}
+            <pre></pre>
+            <button onClick={toggleHouseInfo202}>
+                {isHouseInfo202Visible ? 'ปิดข้อมูล 204' : 'เปิดข้อมูล 204'}
+            </button>
+            {isHouseInfo202Visible && <HouseInfo204 />}
+            <pre></pre>
+            <button onClick={toggleHouseInfo202}>
+                {isHouseInfo202Visible ? 'ปิดข้อมูล 205' : 'เปิดข้อมูล 205'}
+            </button>
+            {isHouseInfo202Visible && <HouseInfo205 />}
+            <pre></pre>
+            <button onClick={toggleHouseInfo202}>
+                {isHouseInfo202Visible ? 'ปิดข้อมูล 206' : 'เปิดข้อมูล 206'}
+            </button>
+            {isHouseInfo202Visible && <HouseInfo206 />}
             <pre></pre>
         </div>
     );
