d4ae0f080c61a4cd9a1310d1ad482a260a4b5290
Supunsapoon <supunsapoon@gmail.com>
2024-07-25 14:54:16 +0700

    First time to commit

--- src/components/HouseInfo205.tsx
+++ src/components/HouseInfo205.tsx
@@ -34,7 +34,7 @@ const HouseInfo205 = () => {
     // Fetch invoices from Firestore
     const fetchInvoices = async () => {
         try {
-            const querySnapshot = await getDocs(collection(db, "invoices5")); // Use a different collection name
+            const querySnapshot = await getDocs(collection(db, "invoices205")); // Use a different collection name
             const invoicesData: Invoice[] = [];
             querySnapshot.forEach((doc) => {
                 invoicesData.push({ id: doc.id, ...doc.data() } as Invoice);
