a2e69f9be04dcad7c89056f093e92ce4bb3acb04
Supunsapoon <supunsapoon@gmail.com>
2024-07-26 07:17:27 +0700

    Updates

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/roomsalaya/',  // เปลี่ยน '/roomsalaya/' ให้ตรงกับชื่อ repository ของคุณ
-  plugins: [react()],
-});
